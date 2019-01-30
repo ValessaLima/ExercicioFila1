1adf445e315720c3d00c052aa6e6592bbd429782
Valessa Lima <44641468+ValessaLima@users.noreply.github.com>
2019-01-30 16:36:59 -0300

    questão2

---
+++ questão2.py
@@ -0,0 +1,27 @@
+
+class Fila():]
+
+  def __init__(self):
+    self.lista = []
+
+  def Peek(self):
+    tamanho= len(self.lista)
+    retur self.lista[0]
+
+  def enqueue(self, quantitade):
+    self.lista.append(quantidade)
+
+  def dequeue(self):
+    if self.isEmpty():
+      return false
+    return self.lista.pop()
+
+  def length (self):
+    return len(self.lista)
+
+  def isEmpty(self):
+    return len(self.lista)==0
+
+Lista= Fila()
+Lista.enqueue("valessa")
+print(Lista.lista)  
